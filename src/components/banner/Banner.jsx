a3efb2768df5a8c05280a8040b437196cddf0bd9
Md.Tanvir <hasanmdtanvir916@gmail.com>
2025-05-13 03:06:12 +0600

    wait for deploy

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -5,7 +5,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
-import "./Banner.css";
+import "../../components/banner/banner.css";
 
 const slides = [
   {
